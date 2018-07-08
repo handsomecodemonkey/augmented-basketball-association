f17018158e6665c1ae178edc2f71a007ff5b0b3f
Erick Kusnadi <erick@handsomecodemonkey.com>
2018-07-07 21:48:13 -0700

    added token tests

--- test/TestABAToken.js
+++ test/TestABAToken.js
@@ -4,8 +4,8 @@ contract('ABAToken JS Test', async(accounts) => {
 
 	let abaToken;
 
-	before(async() => {
-		abaToken = await ABAToken.deployed();
+	beforeEach(async() => {
+		abaToken = await ABAToken.new();
 	});
 
 	it("should have total supply of 100", async() => {
@@ -16,6 +16,34 @@ contract('ABAToken JS Test', async(accounts) => {
 	it("should give total supply to first account", async() => {
 		let balance = await abaToken.balanceOf(accounts[0]);
 		assert.equal(balance.valueOf(), 100);
+	});
+
+	it("should transfer tokens", async() => {
+		await abaToken.transfer(accounts[1], 50, {from: accounts[0]});
+		let balanceOfFirstAccount = await abaToken.balanceOf(accounts[0]);
+		let balanceOfSecondAccount = await abaToken.balanceOf(accounts[1]);
+
+		assert.equal(balanceOfFirstAccount.valueOf(), 50);
+		assert.equal(balanceOfSecondAccount.valueOf(), 50);
+	});
+
+	it("should not allow illegal over balance transfers", async() => {
+		try{
+			await abaToken.transfer(accounts[1], 200, {from: accounts[0]});
+			assert.fail(); //Should throw exception
+		} catch(error) {
+			assert.isTrue(error.message.includes("revert"));
+		}
+	});
+
+	it("should not allow double transfers", async() => {
+		try{
+			await abaToken.transfer(accounts[1], 100, {from: accounts[0]});
+			await abaToken.transfer(accounts[1], 100, {from: accounts[0]});
+			assert.fail(); //Should throw exception
+		} catch(error) {
+			assert.isTrue(error.message.includes("revert"));
+		}
 	});
 
 });
