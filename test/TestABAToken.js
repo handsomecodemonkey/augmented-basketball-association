b1c82c6063cfcfcd3200eed24f7c41affe69afc2
Erick Kusnadi <erick@handsomecodemonkey.com>
2018-07-07 21:15:59 -0700

    Add before function to ABAToken js test

--- test/TestABAToken.js
+++ test/TestABAToken.js
@@ -2,9 +2,19 @@ const ABAToken = artifacts.require("ABAToken");
 
 contract('ABAToken JS Test', async(accounts) => {
 
+	let abaToken;
+
+	before(async() => {
+		abaToken = await ABAToken.deployed();
+	});
+
+	it("should have total supply of 100", async() => {
+		let totalSupply = await abaToken.totalSupply();
+		assert.equal(totalSupply, 100);
+	});
+
 	it("should give total supply to first account", async() => {
-		let abaToken = await ABAToken.deployed();
-		let balance = await abaToken.balanceOf.call(accounts[0]);
+		let balance = await abaToken.balanceOf(accounts[0]);
 		assert.equal(balance.valueOf(), 100);
 	});
 
