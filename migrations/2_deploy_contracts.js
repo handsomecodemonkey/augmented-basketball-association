dab18d45e3fb02c0aad81378cfdeff00527d7fcb
Erick Kusnadi <erick@handsomecodemonkey.com>
2018-07-07 21:04:53 -0700

    updated the migration scripts to use async/await

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,26 +2,16 @@ const SafeMath = artifacts.require("./SafeMath");
 const ABAToken = artifacts.require("./ABAToken");
 var ABADao = artifacts.require("./ABADao");
 
-let abaToken;
-let abaDAO;
-
-module.exports = function(deployer, network) {
+module.exports = async function(deployer, network) {
 	console.log("Deploying to network: " + network);
 
 	deployer.deploy(SafeMath);
 	deployer.link(SafeMath,ABAToken);
 	deployer.deploy(ABAToken);
 
-	deployer
-		.then(() => ABAToken.deployed())
-		.then(tokenInstance => {
-			abaToken = tokenInstance;
-			return ABADao.new(abaToken.address);
-		})
-		.then(abaDAOInstance => {
-			abaDAO = abaDAOInstance;
-			return abaDAO.getBasketBallLeagueStorageContractAddress();
-		}).then(address => {
-			console.log("LeagueStorage Address: " + address);
-		});
+	let abaToken = await ABAToken.deployed();
+	let abaDAO = await ABADao.new(abaToken.address);
+
+	let leageStorageAddress = await abaDAO.getBasketBallLeagueStorageContractAddress();
+	console.log("LeagueStorage Address: " + leageStorageAddress);
 };
