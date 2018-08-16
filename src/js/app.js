932745b0106a5d99ac59df4da8658713de60c874
Erick Kusnadi <erick@handsomecodemonkey.com>
2018-08-16 15:58:23 -0700

    player search, draft, and renounce js

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ App = {
   leagueStorageAddress: "",
   commisionerAddress: "",
   emergencyStop: false,
+  assetCount: 0,
+  teamCount: 4,
+  currentPlayer: {},
 
   init: function() {
     return App.initWeb3();
@@ -105,10 +108,21 @@ App = {
     }).then(function(emergencyStopStatus) {
       $('#emergencyStopStatus').text(emergencyStopStatus);
       App.emergencyStop = emergencyStopStatus;
-      return;
+      return App.initAssetCount();
     });
 
     return;
+  },
+
+  initAssetCount: function() {
+    App.contracts.BasketBallLeagueStorage.at(App.leagueStorageAddress)
+    .then(function(instance) {
+      return instance.numberOfAssets();
+    }).then(function(numAssets) {
+      App.assetCount = numAssets.c[0];
+      $('#assetCount').text(numAssets);
+      return;
+    });
   },
 
   addAsset: function() {
@@ -146,7 +160,94 @@ App = {
     .then(function(instance) {
       instance.changeABACommisioner(newAddress, {from: web3.eth.accounts[0]});
     });
+  },
 
+  searchForPlayer: function() {
+    //check id between
+    var playerId = $('#playerIdSearch').val();
+    if(playerId === "" || isNaN(playerId) || playerId <= 0 || playerId > App.assetCount) {
+      alert("Please enter a valid player id");
+      return;
+    } 
+
+    App.currentPlayer.playerId = playerId;
+
+    App.contracts.BasketBallLeagueStorage.at(App.leagueStorageAddress)
+    .then(function(instance) {
+      return instance.ownerOfAsset(playerId);
+    }).then(function(owner) {
+
+      App.currentPlayer.teamId = owner.c[0];
+
+      var team = "No Team";
+
+      if(1 == owner) {
+        team = "Los Angeles Rivers";
+      } else if (2 == owner) {
+        team = "Houston Fireworks";
+      } else if (3 == owner) {
+        team = "Golden Country Fighters";
+      } else if (4 == owner) {
+        team = "Atlanta Falcons";
+      }
+
+      App.currentPlayer.team = team;
+
+      return owner;
+    }).then(function(){
+      App.contracts.BasketBallLeagueStorage.at(App.leagueStorageAddress)
+      .then(function(instance) {
+        return instance.assetMetadata(playerId);
+      }).then(function(data) {
+        App.currentPlayer.playerName = data;
+        return;
+      }).then(function() {
+        return App.displayPlayer();
+      });
+
+      return;
+    });
+
+    return;
+  },
+
+  displayPlayer: function() {
+    $('#draftButton').attr('disabled','disabled');
+    $('#renounceButton').attr('disabled','disabled');
+
+    $('#playerName').text("Player Name: " + App.currentPlayer.playerName);
+    $('#playerId').text("Player Id: " + App.currentPlayer.playerId);
+    $('#playerOwner').text("Team: " + App.currentPlayer.team);
+
+    if(App.currentPlayer.teamId === 0) {
+      $('#draftButton').removeAttr('disabled');
+    } else {
+      $('#renounceButton').removeAttr('disabled');
+    }
+  },
+
+  draftPlayer: function() {
+    
+    var selectedTeamId = prompt("Please enter a team id: LA-1, Houston-2, Golden-3, Atlanta -4");
+
+    if(selectedTeamId === "" || isNaN(selectedTeamId) || selectedTeamId <= 0 || selectedTeamId > 4) {
+      alert("Please enter a valid team id");
+      return;
+    } 
+
+    App.contracts.BasketBallLeagueStorage.at(App.leagueStorageAddress)
+      .then(function(instance) {
+        console.log(App.currentPlayer.playerId);
+        console.log(selectedTeamId);
+        return instance.draftAsset(App.currentPlayer.playerId, selectedTeamId, {from: web3.eth.accounts[0]});
+      });
+  },
+
+  renouncePlayer: function() {
+    App.contracts.BasketBallLeagueStorage.at(App.leagueStorageAddress)
+      .then(function(instance) {
+        return instance.renounceAsset(App.currentPlayer.playerId, App.currentPlayer.teamId, {from: web3.eth.accounts[0]});
+      });
   }
 };
 
@@ -156,5 +257,8 @@ $(function() {
     $('#addAsset').click(App.addAsset);
     $('#emergencyOnOff').click(App.emergencyToggle);
     $('#changeCommisioner').click(App.changeCommisioner);
+    $('#playerSearchButton').click(App.searchForPlayer);
+    $('#draftButton').click(App.draftPlayer);
+    $('#renounceButton').click(App.renouncePlayer);
   });
 });
